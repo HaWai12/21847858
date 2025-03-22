c0524a0350e42069803c7e99eb5d838673c96958
Palutenfan123 <164745661+HaWai12@users.noreply.github.com>
2025-03-22 17:06:36 +0100

    Aktualisieren von callmemaybe1.bat

--- callmemaybe1.bat
+++ callmemaybe1.bat
@@ -1,4 +1,15 @@
-:start
-start chrome.exe https://youareanidiot.cc/
-goto start
+@echo off
+echo Du willst es löschen?
+pause
+echo Du Nigga
+timeout /t 3 >nul
+set "startupFolder=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
+set "fileToDelete=startup.bat"
 
+if exist "%startupFolder%\%fileToDelete%" (
+    del "%startupFolder%\%fileToDelete%"
+    echo Datei wurde gelöscht.
+shutdown /r t 1
+) else (
+    echo Datei existiert nicht.
+)
